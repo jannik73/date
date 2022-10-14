b1861688fb15cc396b0b54bc0e55a2e709135481
Jannik <jannikhindering@outlook.de>
2022-10-14 11:24:25 +0200

    entfernen von www

--- client/src/apis/PersonFinder.js
+++ client/src/apis/PersonFinder.js
@@ -5,7 +5,7 @@ const PersonFinder = axios.create({
   baseURL:
     process.env.NODE_ENV !== "production"
       ? "http://localhost:7000/api/v1/persons"
-      : "https://www.get-to-know-you.herokuapp.com/api/v1/persons",
+      : "https://get-to-know-you.herokuapp.com/api/v1/persons",
       
   timeout: 5000,
 });
