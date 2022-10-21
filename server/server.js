7dc2e21889343973042c7479541487b8451ccfae
Jannik <jannikhindering@outlook.de>
2022-10-21 18:13:26 +0200

    change html

--- server/server.js
+++ server/server.js
@@ -125,7 +125,7 @@ app
 
   // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.js'));
 });
 
 app.listen(PORT, () => console.log("Magic happening on PORT", +PORT));
