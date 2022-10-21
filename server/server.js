89c746dd31e3ae74d627d9ad9476a84d8b836b2f
Jannik <jannikhindering@outlook.de>
2022-10-21 18:15:01 +0200

    change html

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ app.use(cors());
 
 // Have Node serve the files for our built React app
 //app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/src')));
 
 app
   .route("/api/v1/persons")
